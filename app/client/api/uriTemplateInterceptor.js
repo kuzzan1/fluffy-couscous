b0545ec91e6d58781d70e942d8d5895c0b6fba66
jonas <jonas.lund@mobenga.com>
2017-06-13 16:23:20 +0200

    started with fixutres

--- app/client/api/uriTemplateInterceptor.js
+++ app/client/api/uriTemplateInterceptor.js
@@ -6,12 +6,12 @@ define(function(require) {
 	return interceptor({
 		request: function (request /*, config, meta */) {
 			/* If the URI is a URI Template per RFC 6570 (http://tools.ietf.org/html/rfc6570), trim out the template part */
-			if (request.path.indexOf('{') === -1) {
+		//	if (request.path.indexOf('{') === -1) {
 				return request;
-			} else {
-				request.path = request.path.split('{')[0];
-				return request;
-			}
+		//	} else {
+		//		request.path = request.path.split('{')[0];
+		//		return request;
+		//	}
 		}
 	});
 
